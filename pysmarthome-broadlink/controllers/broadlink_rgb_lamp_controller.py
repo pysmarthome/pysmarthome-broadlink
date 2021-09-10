6197740f93e801490f89d78a4602e0717bafc17e
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-10 01:22:01 -0300

    update BroadlinkRgbLamp to conform pysmarthome 2.1.0

--- pysmarthome-broadlink/controllers/broadlink_rgb_lamp_controller.py
+++ pysmarthome-broadlink/controllers/broadlink_rgb_lamp_controller.py
@@ -1,26 +1,6 @@
-from pysmarthome import RgbLampController
+from pysmarthome import MultiCommandRgbLampController
 from .broadlink_controller import BroadlinkDeviceController
-from ..models import BroadlinkRgbLampsModel
-
-class BroadlinkRgbLampController(BroadlinkDeviceController, RgbLampController):
-    model_class = BroadlinkRgbLampsModel
 
 
-    def on(self):
-        self.send_command('on')
-        return True
-
-
-    def off(self):
-        self.send_command('off')
-        return True
-
-
-    def set_brightness(self):
-        pass
-
-
-    def set_color(self, color_id):
-        self.send_command(color_id)
-        self.set_state(color=color_id)
-        return True
+class BroadlinkRgbLampController(BroadlinkDeviceController, MultiCommandRgbLampController):
+    model_class = MultiCommandRgbLampController.model_class
